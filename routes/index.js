04900ecb5100134b09e5a03a8f0762657ec0eeb7
dani71296 <israel.tudela712.96@gmail.com>
2026-07-03 11:59:36 -0600

    prueba final

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,8 @@ const router = express.Router();
 
 // Redirige cualquier petición que empiece con /contacts al archivo contacts.js
 router.use('/contacts', require('./contacts'));
+router.get('/', (req, res) => {
+    res.send('Contacts API funcionando en Render 🚀');
+});
 
 module.exports = router;
